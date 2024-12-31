d155f94d09def7834cf4f8824cf29f3389b01328
vamika-muthukumar <vamikamuthukumar147@gmail.com>
2024-12-31 20:53:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional script for smooth scroll effect on clicking nav links
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
